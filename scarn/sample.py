ed62905694a985517232927ea62a0988167ae05d
Yu Yu4 Yan <yanyu4@Lenovo.com>
2020-01-28 07:24:55 +0800

    contniue

--- scarn/sample.py
+++ scarn/sample.py
@@ -21,7 +21,7 @@ def parse_args():
     parser.add_argument("--sample_dir", type=str, default="./sample")
     parser.add_argument("--test_data_dir", type=str, default="dataset/DIV2K/DIV2K_valid")
     parser.add_argument("--cuda", action="store_true")
-    parser.add_argument("--scale", type=int, default=4)
+    parser.add_argument("--scale", type=int, default=0)
     parser.add_argument("--shave", type=int, default=20)
 
     return parser.parse_args()
@@ -37,8 +37,7 @@ def save_image(tensor, filename):
     im.save(filename)
 
 
-def sample(net, device, dataset, cfg):
-    scale = cfg.scale
+def sample(net, device, dataset, cfg, scale):
     for step, (hr, lr, name) in enumerate(dataset):
         if "DIV2K" in dataset.name:
             t1 = time.time()
@@ -53,7 +52,7 @@ def sample(net, device, dataset, cfg):
             lr_patch[3].copy_(lr[:, h-h_chop:h, w-w_chop:w])
             lr_patch = lr_patch.to(device)
             
-            sr = net(lr_patch, cfg.scale).detach()
+            sr = net(lr_patch, scale).detach()
             
             h, h_half, h_chop = h*scale, h_half*scale, h_chop*scale
             w, w_half, w_chop = w*scale, w_half*scale, w_chop*scale
@@ -68,7 +67,7 @@ def sample(net, device, dataset, cfg):
         else:
             t1 = time.time()
             lr = lr.unsqueeze(0).to(device)
-            sr = net(lr, cfg.scale).detach().squeeze(0)
+            sr = net(lr, cale).detach().squeeze(0)
             lr = lr.squeeze(0)
             t2 = time.time()
         
@@ -76,12 +75,12 @@ def sample(net, device, dataset, cfg):
         sr_dir = os.path.join(cfg.sample_dir,
                               model_name, 
                               cfg.test_data_dir.split("/")[-1],
-                              "x{}".format(cfg.scale),
+                              "x{}".format(scale),
                               "SR")
         hr_dir = os.path.join(cfg.sample_dir,
                               model_name, 
                               cfg.test_data_dir.split("/")[-1],
-                              "x{}".format(cfg.scale),
+                              "x{}".format(scale),
                               "HR")
         
         os.makedirs(sr_dir, exist_ok=True)
@@ -115,9 +114,13 @@ def main(cfg):
     
     device = torch.device("cpu")
     net = net.to(device)
-    
-    dataset = TestDataset(cfg.test_data_dir, cfg.scale)
-    sample(net, device, dataset, cfg)
+    if cfg.scale > 0:
+        dataset = TestDataset(cfg.test_data_dir, cfg.scale)
+        sample(net, device, dataset, cfg, cfg.scale)
+    else:
+        for scale in [2, 4]:
+            dataset = TestDataset(cfg.test_data_dir, scale)
+            sample(net, device, dataset, cfg, scale)
  
 
 if __name__ == "__main__":
